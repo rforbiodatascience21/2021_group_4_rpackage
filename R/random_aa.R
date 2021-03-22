a777175a25cd7b379409a8f474c2e6e47e5735d7
yogimolbio <yogeshbasavaraju21@gmail.com>
2021-03-22 10:21:17 +0000

    delete by yogbas

--- R/random_aa.R
+++
@@ -1,7 +0,0 @@
-# R/random_aa.R
-
-random_aa <- function(amino_acid){
-  amino_acid <- sample(c("G","A","V","L","I","M","F","W","P","S","T","C","Y","N","Q","D","E","K","R","H"), size = l, replace = TRUE)
-  aa = paste0(amino_acid, collapse = "")
-  return(aa)
-}
