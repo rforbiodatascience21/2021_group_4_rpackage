9ca915b53d1d3eb4129363efe941a7dd71075eaf
yogimolbio <yogeshbasavaraju21@gmail.com>
2021-03-22 09:36:44 +0000

    Update title

--- R/random_aa.R
+++ R/random_aa.R
@@ -1,4 +1,4 @@
-# R/aa_properties.R
+# R/random_aa.R
 
 random_aa <- function(amino_acid){
   amino_acid <- sample(c("G","A","V","L","I","M","F","W","P","S","T","C","Y","N","Q","D","E","K","R","H"), size = l, replace = TRUE)
