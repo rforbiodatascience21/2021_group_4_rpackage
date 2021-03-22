8e0ecf50d796754c3fe12c00c29570d5b642e569
skosk-dtu <skosk@dtu.dk>
2021-03-22 09:10:54 +0000

    Added chemisty_aa script

---
+++ R/chemistry_aa.R
@@ -0,0 +1,14 @@
+#
+chemistry_aa <- function(amino_acid){
+  chemisty_table <- c("A" = "Hydrophobic", "C" = "Polar", "D" = "Acidic", 
+                      "E" = "Acidic", "F" = "Hydrophobic", "G" = "Polar", 
+                      "H" = "Basic", "I" = "Hydrophobic", "K" = "Basic", 
+                      "L" = "Hydrophobic", "M" = "Hydrophobic", "N" = "Neutral",
+                      "P" = "Hydrophobic", "Q" = "Neutral", "R" = "Basic", 
+                      "S" = "Polar", "T" = "Polar", "V" = "Hydrophobic", 
+                      "W" = "Hydrophobic", "Y" = "Polar")
+  chemistry <- paste(chemisty_table[amino_acid])
+  return(chemistry)
+}
+
+
